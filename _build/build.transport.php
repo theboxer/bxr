893fcc234ebfe61a5a534fa6605689894d3ae76c
Travis CI <pecajan+travis@gmail.com>
2014-09-25 01:01:38 +0000

    Bump new dev version

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -15,7 +15,7 @@ set_time_limit(0);
 define('PKG_NAME','BXR');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 define('PKG_VERSION','1.0.0');
-define('PKG_RELEASE','dev-140925010037');
+define('PKG_RELEASE','dev-140925010138');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
